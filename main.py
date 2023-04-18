aaab4b33c0dcca809b521ff3da086d859aeb5a86
JadisHee <hmx>
2023-04-18 11:27:05 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import serial
+import time
+
+# 串口配置
+try:
+    ser = serial.Serial('/dev/ttyUSB4', 115200, timeout=1)
+    print('串口连接成功')
+except:
+    print('串口连接失败')
+    exit()
+
+# 发送数据
+def send_data(data):
+    ser.write(data)
+    print("发送数据: ", data.hex())
+
+    while True:
+        response = ser.readline().hex()
+        if response:
+            print("接收数据: ", response)
+            break
+        time.sleep(0.1)
+
+
+
+# 主函数
+if __name__ == '__main__':
+
+    data = "77 04 00 59 5D"
+    # 发送数据并等待接收回复
+    while True:
+        send_data(data)
+        time.sleep(1)
